86f394737e7a32a0cd5cea1bbc258a8d2e819a95
William White <william.white86@gmail.com>
2020-02-22 19:49:27 -0500

    added routing for about, contact, and portfolio pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Body from './components/Body';
+import About from './components/pages/About';
+import Portfolio from './components/pages/Portfolio';
+import Contact from './components/pages/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Footer />
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Route exact path="/" component = {About} />
+        <Route exact path="/portfolio" component = {Portfolio}/>
+        <Route exact path="/contact" component = {Contact}/>
+        <Body />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
